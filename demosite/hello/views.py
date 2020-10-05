896b34a19a0c03f4ac3a82a3be2314172884e629
David Anderson <david.anderson@redhat.com>
2020-10-05 10:35:15 -0500

    Update views.py

--- demosite/hello/views.py
+++ demosite/hello/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello! Demo of image version management.")
+    return HttpResponse("Hello! Demo of image version management. This is a NEW verion 0.2")
 
